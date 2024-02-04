cbc0a61ee077de5bc01819e59d80f93b4eb97b72
AyushiAnand70047 <ayushianand088@gmail.com>
2024-02-04 09:08:10 +0530

    React Js

---
+++ basic-react-app/src/Product.jsx
@@ -0,0 +1,16 @@
+import "./Product.css";
+
+function Product({title, price=100, features, features2}){
+    return (
+        <div className="Product">
+            <h1> { title } </h1>
+            <h4> Price: { price } </h4>
+            <p>{features}</p>
+            <p>{features && features.join(", ")}</p>
+            {/* we can't directly render object but we can render individual key  */}
+            {/* <p>{features2.a}</p> */}
+        </div>
+    );
+}
+
+export default Product;
